817debdfb976896a61dccf995c2d368a6d940cff
J.Y Han <ihanjiyun@gmail.com>
2014-09-02 14:23:47 +0800

    init

---
+++ app/src/javascripts/app.js
@@ -0,0 +1,12 @@
+/*global $, Modernizr, require, _, __dirname, module, classie, jQuery, globalData, NProgress, ga,*/
+
+/*
+* @Author: hanjiyun
+* @Date:   2014-07-05 16:45:59
+* @Last Modified by:   hanjiyun
+* @Last Modified time: 2014-07-05 16:47:03
+*/
+
+$(function () {
+    console.log('successful!');
+});
